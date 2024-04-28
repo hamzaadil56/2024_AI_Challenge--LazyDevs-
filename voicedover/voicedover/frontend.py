5f4be01f94c66680316b9550a149c9451a8289dc
Muhammad <hamzaadil56@gmail.com>
2024-04-28 18:26:40 +0500

    built backend apis

--- voicedover/voicedover/frontend.py
+++ voicedover/voicedover/frontend.py
@@ -2,21 +2,37 @@ import streamlit as st
 import sounddevice as sd
 import soundfile as sf
 import os
+from datetime import datetime
+import requests
+from requests.models import Response
 
 # Set up Streamlit layout
 st.title("Audio Recorder")
 
 # Define function to record audio
-def record_audio(filename, duration=5, sample_rate=44100):
-    # Record audio
-    audio_data = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=2)
-    sd.wait()
+
+
+def record_audio(filename, duration=10, sample_rate=44100):
+    # Display recording status
+    with st.empty():
+        st.write("Recording...")
+        # Record audio
+        audio_data = sd.rec(int(duration * sample_rate),
+                            samplerate=sample_rate, channels=2)
+        sd.wait()
+        # Notify user that recording has stopped
+        st.write("Compiling...")
 
     # Save audio to file
     sf.write(filename, audio_data, sample_rate)
+    # Clear the compiling message
+    st.empty()
+
 
 # Display user input for filename and duration
-filename = st.text_input("Enter filename to save audio:", "output.wav")
+default_filename = f"voicedover/output_{
+    datetime.now().strftime('%Y%m%d_%H%M%S')}.wav"
+filename = st.text_input("Enter filename to save audio:", default_filename)
 duration = st.slider("Recording duration (seconds):", 1, 10, 5)
 
 # Display record button
@@ -28,6 +44,21 @@ if st.button("Record"):
         # Record audio and save to file
         record_audio(filename, duration)
         st.success(f"Audio recorded and saved as '{filename}'.")
+
+        # Make a GET request to a server
+        response: Response = requests.get(
+            f"http://127.0.0.1:8000/audio?audio_file_path={filename}")
+        st.write(response.text)
+        if response.text:
+            # Make a POST request to a server adding a body of prompt property
+            # with the response text
+            # Prepare the data to be sent in the request body
+            post_data = {"prompt": response.text}
+
+            response = requests.post(
+                # Use the 'data' parameter to send the request body
+                "http://127.0.0.1:8000/call-api", json=post_data)
+            st.write(response.text)
 
 # Display folder path where the audio will be saved
 st.write(f"Audio will be saved in: `{os.getcwd()}`")
--- voicedover/voicedover/main.py
+++ voicedover/voicedover/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from openai import OpenAI
 from dotenv import load_dotenv
-from voicedover.openai_assistant import OpenAIAssistant
 import os
+from pydantic import BaseModel
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
@@ -13,46 +13,301 @@ client = OpenAI(
 
 app = FastAPI()
 
-openai_assistant = OpenAIAssistant(client=client)
+json_schema = {
+    "openapi": "3.1.0",
+    "info": {
+        "title": "FastAPI",
+        "version": "0.1.0"
+    },
+    "servers": [
+        {
+            "url": "link yo yout website goes here"
+        }
+    ],
+    "paths": {
+        "/create": {
+            "post": {
+                "summary": "Create Todo",
+                "description": "Creates a new todo item.\n\n:param text: The text of the todo item.\n:type text: str\n:param is_complete: Whether the todo item is complete. Defaults to False.\n:type is_complete: bool\n:return: A dictionary containing the text of the new todo item.\n:rtype: dict\n\n:raises ValueError: If the text is empty.",
+                "operationId": "create_todo_create_post",
+                "parameters": [
+                    {
+                        "name": "text",
+                        "in": "query",
+                        "required": True,
+                        "schema": {
+                            "type": "string",
+                            "title": "Text"
+                        }
+                    },
+                    {
+                        "name": "is_complete",
+                        "in": "query",
+                        "required": False,
+                        "schema": {
+                            "type": "boolean",
+                            "default": False,
+                            "title": "Is Complete"
+                        }
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "Successful Response",
+                        "content": {
+                            "application/json": {
+                                "schema": {}
+                            }
+                        }
+                    },
+                    "422": {
+                        "description": "Validation Error",
+                        "content": {
+                            "application/json": {
+                                "schema": {
+                                    "$ref": "#/components/schemas/HTTPValidationError"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        "/todo": {
+            "get": {
+                "summary": "Get All Todos",
+                "description": "Returns a list of all todo items.\n\n:return: A list of todo items.\n:rtype: list\n\n:Example:\n\n>>> get_all_todos()\n[{'id': 1, 'text': 'Buy milk', 'is_done': False}, {'id': 2, 'text': 'Wash dishes', 'is_done': False}]",
+                "operationId": "get_all_todos_todo_get",
+                "responses": {
+                    "200": {
+                        "description": "Successful Response",
+                        "content": {
+                            "application/json": {
+                                "schema": {}
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        "/done": {
+            "get": {
+                "summary": "List Done Todos",
+                "description": "Returns a list of all done todo items.\n\n:return: A list of done todo items.\n:rtype: list\n\n:Example:\n\n>>> list_done_todos()\n[{'id': 1, 'text': 'Buy milk', 'is_done': True}, {'id': 3, 'text': 'Buy butter', 'is_done': True}]",
+                "operationId": "list_done_todos_done_get",
+                "responses": {
+                    "200": {
+                        "description": "Successful Response",
+                        "content": {
+                            "application/json": {
+                                "schema": {}
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        "/update/{id}": {
+            "patch": {
+                "summary": "Update Todo",
+                "description": "Updates a todo item.\n\n:param id: Id of the todo item to update.\n:type id: int\n:param text: New text of the todo item(optional).\n:type text: str\n:param is_complete: Completion status of the todo item (optional).\n:type is_complete: bool\n:return: A dictionary containing the id of the updated todo item",
+                "operationId": "update_todo_update_id_patch",
+                "parameters": [
+                    {
+                        "name": "id",
+                        "in": "path",
+                        "required": True,
+                        "schema": {
+                            "type": "integer",
+                            "title": "Id"
+                        }
+                    },
+                    {
+                        "name": "text",
+                        "in": "query",
+                        "required": True,
+                        "schema": {
+                            "type": "string",
+                            "title": "Text"
+                        }
+                    },
+                    {
+                        "name": "is_complete",
+                        "in": "query",
+                        "required": False,
+                        "schema": {
+                            "type": "boolean",
+                            "title": "Is Complete"
+                        }
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "Successful Response",
+                        "content": {
+                            "application/json": {
+                                "schema": {}
+                            }
+                        }
+                    },
+                    "422": {
+                        "description": "Validation Error",
+                        "content": {
+                            "application/json": {
+                                "schema": {
+                                    "$ref": "#/components/schemas/HTTPValidationError"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        "/delete/{id}": {
+            "delete": {
+                "summary": "Delete Todo",
+                "description": "Deletes a todo item.\n\n:param id: The id of the todo item to delete.\n:type id: int\n:return: A dictionary containing the id of the deleted todo item.\n:rtype: dict\n\n:Example:\n\n>>> delete_todo(1)\n{'todo deleted': 1}",
+                "operationId": "delete_todo_delete_id_delete",
+                "parameters": [
+                    {
+                        "name": "id",
+                        "in": "path",
+                        "required": True,
+                        "schema": {
+                            "type": "integer",
+                            "title": "Id"
+                        }
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "Successful Response",
+                        "content": {
+                            "application/json": {
+                                "schema": {}
+                            }
+                        }
+                    },
+                    "422": {
+                        "description": "Validation Error",
+                        "content": {
+                            "application/json": {
+                                "schema": {
+                                    "$ref": "#/components/schemas/HTTPValidationError"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    },
+    "components": {
+        "schemas": {
+            "HTTPValidationError": {
+                "properties": {
+                    "detail": {
+                        "items": {
+                            "$ref": "#/components/schemas/ValidationError"
+                        },
+                        "type": "array",
+                        "title": "Detail"
+                    }
+                },
+                "type": "object",
+                "title": "HTTPValidationError"
+            },
+            "ValidationError": {
+                "properties": {
+                    "loc": {
+                        "items": {
+                            "anyOf": [
+                              {
+                                  "type": "string"
+                              },
+                                {
+                                  "type": "integer"
+                              }
+                            ]
+                        },
+                        "type": "array",
+                        "title": "Location"
+                    },
+                    "msg": {
+                        "type": "string",
+                        "title": "Message"
+                    },
+                    "type": {
+                        "type": "string",
+                        "title": "Error Type"
+                    }
+                },
+                "type": "object",
+                "required": [
+                    "loc",
+                    "msg",
+                    "type"
+                ],
+                "title": "ValidationError"
+            }
+        }
+    }
+}
 
-audio_file_path = './EarningsCall.wav'
 
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "get_api_details",
+            "description": "Get all the details of the api",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "url": {
+                        "type": "string",
+                        "description": "This is the url of the api to be called",
+                    },
+                    "method": {
+                        "type": "string",
+                        "enum": ["get", "post", "put", "delete"],
+                        "description": "This is the method of the api like 'post' 'get' 'put' 'delete'.",
+                    },
+                },
+                "required": ["url", "method"],
+            },
+        }
+    },
+]
 
-@app.get("/{prompt}")
-def get_response(prompt):
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-            {"role": "user", "content": prompt}
-        ]
-    )
-
-    return completion.choices[0].message
+messages = [
+    {"role": "system",
+     "content": f'''You are an API extracter which extracts API's from the given schema
+     {json_schema} according to the context that what user wanted to do. Like for example: User says "Add Breakfast in my todo list" or "I want todo homework" You need to understand the context of the user and then pull out the write api for that action from the given schema. If you are not sure then ask more details from the user!
+     You should not any link from yourself. It should be according to the schema. Ask more questions if you can't understand.
+     '''},
+]
 
 
 @app.get("/audio")
 def transcribe_audio(audio_file_path):
     with open(audio_file_path, 'rb') as audio_file:
         transcription = client.audio.transcriptions.create(
-            "whisper-1", audio_file)
-    print(transcription)
-    return transcription['text']
-
-# Example usage:
-# python main.py
-# following function will take in input from the user and send it to openAi client. The response will then be displayed.
+            file=audio_file, model='whisper-1')
+    return transcription.text
 
 
-@app.get("/chat")
-def chat():
-    user_input = input("Enter your message: ")
-    response = client.chat.completions.create(
+class Prompt(BaseModel):
+    prompt: str
+
+
+@app.post("/call-api")
+def get_response(prompt: Prompt):
+    messages.append({"role": "user", "content": prompt.prompt})
+    completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": user_input}
-        ]
+        messages=messages,
+        tools=tools,
     )
-    print(response.choices[0].message.content)
-    return response.choices[0].message.content
+
+    return completion.choices[0].message
